9eb0a951c6a711a54e2fb24e93758e5edeb954ad
Patrik Drhlík <patrioscraper@gmail.com>
2025-07-08 21:01:42 +0200

    Add iOS SwiftUI app skeleton

---
+++ MaplibreNativeIosSwiftUI/MaplibreNativeIosSwiftUI/MaplibreNativeIosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaplibreNativeIosSwiftUIApp.swift
+//  MaplibreNativeIosSwiftUI
+//
+//  Created by Patrik D on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MaplibreNativeIosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaplibreNativeIosSwiftUI/MaplibreNativeIosSwiftUITests/MaplibreNativeIosSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MaplibreNativeIosSwiftUITests.swift
+//  MaplibreNativeIosSwiftUITests
+//
+//  Created by Patrik D on 08.07.2025.
+//
+
+import Testing
+@testable import MaplibreNativeIosSwiftUI
+
+struct MaplibreNativeIosSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
